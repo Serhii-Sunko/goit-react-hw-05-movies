ff9988306efbb694fa852a9ca96ffa69edc3ceb8
Serhii-Sunko <sergeysunko01@gmail.com>
2022-06-22 14:20:02 +0300

    do fix

---
+++ src/api/constants.js
@@ -0,0 +1,2 @@
+export const BASE_URL = 'https://api.themoviedb.org/3';
+export const API_KEY = 'b0de889e3f741ea781569353e4581a9b';
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import 'modern-normalize/modern-normalize.css';
+import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './Components/App';
+import { BrowserRouter } from 'react-router-dom';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
